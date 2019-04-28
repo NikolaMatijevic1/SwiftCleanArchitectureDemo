6edafba43d4c376446fb6d668981c5697afcc47e
Nikola Matijevic <nikolamatijevic101@gmail.com>
2019-04-28 16:36:48 +0200

    Initial Commit

---
+++ CleanArchitectureDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CleanArchitectureDemo
+//
+//  Created by Nikola Matijevic on 28/04/2019.
+//  Copyright © 2019 Nikola Matijevic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
